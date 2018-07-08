7fc97d389962018a5f5854cc40b59d65470a6587
katy@karstaway.com <katy@karstaway.com>
2018-07-08 12:36:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ function viewCart() {
 
 function total() {
   var sum = 0
-for (var l = 0; l < cart.length +1; l++) {
-  sum += cart[l].itemPrice;
+for (var l = 0; l < cart.length; l++) {
+  sum += cart[l].itemPrice
   return sum
 }}
 
